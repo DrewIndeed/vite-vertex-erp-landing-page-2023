a4c81dd4e38a67fbc18ce3068898ead0b8f6b4e5
Andrew Le Nguyen <anle.drewindeed@gmail.com>
2023-12-28 17:24:07 +0700

    refactor: Hero icons

--- src/components/sections/Features/index.tsx
+++ src/components/sections/Features/index.tsx
@@ -3,19 +3,22 @@
 const Features = () => {
   return (
     <section className="mt-0 md:mt-20">
+      {/* features title and subtitle */}
       <div className="max-w-2xl mx-auto text-center mb-8 lg:mb-10 px-4">
         <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
-          Find the right kind of service
+          Find the right service to succeed
         </h2>
         <p className="mt-1 text-gray-600 dark:text-gray-400">
           Our diversity of support never seizes to amaze
         </p>
       </div>
+      {/* features show case boxes */}
       <div className="w-screen flex justify-center">
         <div
           className="rounded-md md:rounded-xl mx-4 mb-5 flex-1 max-w-[85rem]
         bg-gradient-to-tl from-blue-600/50 to-violet-600/50 dark:from-blue-600/50 dark:to-violet-600/50 p-0 md:p-[2px]"
         >
+          {/* features grid */}
           <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-8 md:gap-12 bg-white/90 dark:bg-[#03020395] backdrop-blur-sm p-5 md:p-10 rounded-md md:rounded-xl">
             <div>
               <div className="relative flex justify-center items-center w-12 h-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl dark:bg-slate-900">
@@ -247,6 +250,7 @@ const Features = () => {
               </div>
             </div>
           </div>
+          {/* features grid */}
         </div>
       </div>
     </section>
