31664c65f55c7cb4d94b7c393f5dd13043536690
Andrew Le Nguyen <anle.drewindeed@gmail.com>
2023-12-29 13:57:40 +0700

    refactor: testimonials constants

---
+++ src/components/sections/Testinomials/constant.tsx
@@ -0,0 +1,29 @@
+export const TESTIMONIALS = [
+  {
+    content: `" With VertexERP, we're able to easily track our performance
+in full detail. It's become an essential tool for us to grow
+and engage with our audience. "`,
+    image:
+      "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+    name: "Josh Tyson",
+    job: "Product Manager | Capsule",
+  },
+  {
+    content: `" In September, I will be using this tool for 2 years. I went
+    through multiple updates and changes and I'm very glad to see
+    the consistency and effort made by the team. "`,
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+    name: "Luisa",
+    job: "Senior Director of Operations | Fitbit",
+  },
+  {
+    content: `" Refreshing and Thought provoking design and it changes my
+    view about how I utilize cloud services. Great typography,
+    modern clean white design, nice tones of the color. "`,
+    image:
+      "https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80",
+    name: "Alisa Williams",
+    job: "Entrepreneur | Happy customer",
+  },
+];
